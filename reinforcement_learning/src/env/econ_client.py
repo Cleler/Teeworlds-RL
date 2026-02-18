3a43108182754505ca265e81ac70e2a585f365e8
Léandre Coussy <coussy.leandre@gmail.com>
2026-02-18 20:46:42 +0100

    try with binding w to jump

--- reinforcement_learning/src/env/econ_client.py
+++ reinforcement_learning/src/env/econ_client.py
@@ -342,7 +342,7 @@ class EconClient:
 
             response = self._recv_until("Authentication", timeout=5.0)
             if "Authentication successful" in response:
-                self._send("bind space +jump")
+                self._send("bind w +jump")
                 self.sock.settimeout(self.read_timeout)
                 return True
             return False
